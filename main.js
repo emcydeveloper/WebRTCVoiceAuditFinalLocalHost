467fa132bdd9ba315514b4d265a78e6a5a35a7d9
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-03-29 16:33:01 +0530

    commit

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-/* global TimelineDataSeries, TimelineGraphView */
-
 "use strict";
 
 const audio2 = document.querySelector("audio#audio2");
